f9a401c344af688c5d9aaeaefca1792f2003ceda
Messias Martins <messiasmartins@outlook.com>
2022-09-22 09:03:26 -0300

    Rotas nomeadas

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,14 @@ use App\Http\Controllers\PrestadorController;
 use App\Http\Controllers\TomadorController;
 use App\Http\Controllers\NotaController;
 
-Route::get('/', [PrestadorController::class, 'index']);
-Route::get('/prestadores', [PrestadorController::class, 'create']);
-Route::post('/prestadores', [PrestadorController::class, 'store']);
-Route::delete('/prestadores/{id}', [PrestadorController::class, 'destroy']);
+Route::get('/', [PrestadorController::class, 'index'])->name('prestador.index');
+Route::get('/prestadores', [PrestadorController::class, 'create'])->name('prestador.create');
+Route::post('/prestadores', [PrestadorController::class, 'store'])->name('prestador.store');
+Route::delete('/prestadores/{id}', [PrestadorController::class, 'destroy'])->name('prestador.destroy');
 
-Route::get('/tomadores/index', [TomadorController::class, 'index']);
-Route::get('/tomadores', [TomadorController::class, 'create']);
-Route::post('/tomadores', [TomadorController::class, 'store']);
-Route::delete('/tomadores/{id}', [TomadorController::class, 'destroy']);
+Route::get('/tomadores/index', [TomadorController::class, 'index'])->name('tomador.index');
+Route::get('/tomadores', [TomadorController::class, 'create'])->name('tomador.create');
+Route::post('/tomadores', [TomadorController::class, 'store'])->name('tomador.store');
+Route::delete('/tomadores/{id}', [TomadorController::class, 'destroy'])->name('tomador.destroy');
 
 Route::get('/notas', [NotaController::class, 'nota']);
